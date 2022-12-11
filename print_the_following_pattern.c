30b44b2c949e949bae132b90f15bf91c550f8066
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2022-12-11 20:57:51 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c=0;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=i;j++)
         {
-            
-            printf("%d ",j);
+            printf("%d",j);
         }
         printf("
 ");
