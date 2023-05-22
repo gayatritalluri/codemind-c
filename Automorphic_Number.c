e821f448682a39ebe050fa3fc7ef13dd56fc9e3c
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2023-05-22 16:33:36 +0530

    #include <stdio.h> int checkAutomorphic(int num){     int sq = num * num;     while(num != 0)     {         if(num % 10 != sq % 10){             return 0;         }         num /= 10;         sq /= 10;     }     return 1; }
    int main ()
    {
        int num, sq = num * num ;
        scanf("%d",&num);
        if(checkAutomorphic(num))
            printf("Automorphic Number");
        else
            printf("Not an Automorphic Number");
        
    
    }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-int main()
-{
-    long int n,sq,a=1;
-    scanf("%d",&n);
-    sq=n*n;
-    while(n!=0)
+#include <stdio.h>
+int checkAutomorphic(int num){
+    int sq = num * num;
+    while(num != 0)
     {
-        if(n%10!=sq%10)
-        {
-            a=0;
-            break;
+        if(num % 10 != sq % 10){
+            return 0;
         }
-        sq=sq/10;
-        n=n/10;
+        num /= 10;
+        sq /= 10;
     }
-    if(a==1)
-    {
+    return 1;
+}
+
+int main ()
+{
+    int num, sq = num * num ;
+    scanf("%d",&num);
+    if(checkAutomorphic(num))
         printf("Automorphic Number");
-    }
     else
-    {
         printf("Not an Automorphic Number");
-    }
+    
+
 }
