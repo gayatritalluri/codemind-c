1ef1d8adb9db5de3d3233c40e1241306742acd1e
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2023-05-22 17:55:02 +0530

    #include<stdio.h> int main(){     int a,b,i,h;     scanf("%d%d",&a,&b);     for (i=1;i<=a&&i<=b;i++){         if(a%i==0 &&b%i==0){             h=i;         }     }     printf("%d",h); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int a,b,i,h;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=a&&i<=b;i++)
-    {
-        if(a%i==0 && b%i==0)
-        {
+    for (i=1;i<=a&&i<=b;i++){
+        if(a%i==0 &&b%i==0){
             h=i;
         }
     }
